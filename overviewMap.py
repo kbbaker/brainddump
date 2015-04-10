f8fd98211e3d1353f8d6ebbe97dfa9dfb77c9c80
kbbaker <kevin.baker@routeyou.com>
2015-04-10 14:13:48 +0200

    playing around
    playing

--- overviewMap.py
+++ overviewMap.py
@@ -2,7 +2,7 @@ __author__ = 'Kevin'
 
 import mapnik
 # Setup the map
-map_canvas = mapnik.Map(width_in_px, height_in_px)
+map_canvas = mapnik.Map(500, 500)
 map_canvas.background = mapnik.Color('rgb(0,0,0,0)') # transparent
 
 # Create a symbolizer to draw the points
@@ -15,9 +15,26 @@ rule.symbols.append(point_symbolizer)
 style.rules.append(rule)
 map_canvas.append_style('GPS_tracking_points', style)
 
+# Create a symbolizer to draw the lines
+style = mapnik.Style()
+rule = mapnik.Rule()
+track_stk = mapnik.Stroke()
+track_stk.width = 5
+line_symbolizer = mapnik.LineSymbolizer(track_stk)
+line_symbolizer.smooth = 1
+rule.symbols.append(line_symbolizer)
+style.rules.append(rule)
+map_canvas.append_style('GPS_tracking_line', style)
+
+# Create a layer to hold the ponts
+layer = mapnik.Layer('GPS_tracking_line')
+layer.datasource = mapnik.Ogr(file="route.kml", layer_by_index=1)
+layer.styles.append('GPS_tracking_line')
+map_canvas.layers.append(layer)
+
 # Create a layer to hold the ponts
 layer = mapnik.Layer('GPS_tracking_points')
-layer.datasource = mapnik.Ogr(file="Laatste_100_km_Parijs_Roubaix_2015.kmz", layer_by_index=0)
+layer.datasource = mapnik.Ogr(file="route.kml", layer_by_index=0)
 layer.styles.append('GPS_tracking_points')
 map_canvas.layers.append(layer)
 
