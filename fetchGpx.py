cb9382234c5e00b1e2a01f8cfa937df4586bbbe6
Kevin Baker <kevin.baker@routeyou.com>
2018-05-22 12:26:30 +0200

    Add files via upload

--- fetchGpx.py
+++ fetchGpx.py
@@ -3,12 +3,12 @@ import gpxpy.parser as parser
 
 url_of_gpx = 'xxxxxxxxxxx'
 
-file_from_url = urllib2.urlopen(url)
+file_from_url = urllib2.urlopen(url_of_gpx)
 
 gpx_file = file_from_url.read()
 
 # parse gpx
-gpx_parser = parser.GPXParser(file)
+gpx_parser = parser.GPXParser(gpx_file)
 gpx_parser.parse()
 
 gpx = gpx_parser.gpx
